de23d42bc4d8a0a65660925bc2efda6064284f2c
admin <brotik13@gmail>
2019-03-07 19:55:43 +0300

    initial commit

---
+++ app/lib.py
@@ -0,0 +1,20 @@
+def create_apartment(rooms, square, price, district):
+    return {
+    'rooms': rooms,
+    'square': square,
+    'price': price,
+    'district': district
+    }
+
+def add_apartment(container, apartment):
+    container.append(apartment)
+
+def search_apartment(container, search):
+    search_lowercased = search.strip().lower()
+    result = []
+    for apartment in container:
+        if search_lowercased in apartment['district'].lower():
+            result.append(apartment)
+            continue
+        if search_lowercased in apartment['price']:
+
